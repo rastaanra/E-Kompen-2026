737953a65668d00f23c8e0b37ec5c722013a9289
oca <mukhammadanshori11@gmail.com>
2026-05-29 11:03:48 +0000

    update api constan

--- lib/core/constants/api_constant.dart
+++ lib/core/constants/api_constant.dart
@@ -1,5 +1,3 @@
 class ApiConstant {
-  // Ganti dengan IP komputer yang menjalankan Laravel
-  // Contoh kalau pakai XAMPP lokal: 'http://192.168.1.x:8000/api'
-  static const String baseUrl = 'http://127.0.0.1:8000/api';
+  static const String baseUrl = 'http://localhost:8000/api';
 }
