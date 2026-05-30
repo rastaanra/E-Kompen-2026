f99d6e115b85bc81e796b29ad8b8bfe32a3eba5b
oca <mukhammadanshori11@gmail.com>
2026-05-30 15:21:54 +0000

    update utils nav admin

---
+++ lib/utils/nav_admin.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import '../views/admin/home_screen.dart';
+import '../views/admin/management/management_view.dart';
+import '../widgets/admin/app_bottom_nav_admin.dart';
+
+// TODO: import ini kalau udah dibuat
+// import '../views/admin/pengajuan_screen.dart';
+// import '../views/admin/profile_screen.dart';
+
+class NavAdmin {
+  static void toHome(BuildContext context) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const AdminHomeScreen()),
+    );
+  }
+
+  static void toPengajuan(BuildContext context) {
+    // TODO: ganti Placeholder dengan AdminPengajuanScreen()
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const Placeholder()),
+    );
+  }
+
+  static void toVerifikasi(BuildContext context) {
+    // TODO: ganti Placeholder dengan AdminVerifikasiScreen()
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const Placeholder()),
+    );
+  }
+
+  static void toManagement(BuildContext context) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const ManagementView()),
+    );
+  }
+
+  static void toProfil(BuildContext context) {
+    // TODO: ganti Placeholder dengan AdminProfileScreen()
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const Placeholder()),
+    );
+  }
+
+  static void handleBottomNav(
+    BuildContext context,
+    NavTabAdmin tab,
+    NavTabAdmin activeTab,
+  ) {
+    if (tab == activeTab) return;
+    switch (tab) {
+      case NavTabAdmin.home:
+        toHome(context);
+        break;
+      case NavTabAdmin.pengajuan:
+        toPengajuan(context);
+        break;
+      case NavTabAdmin.verifikasi:
+        toVerifikasi(context);
+        break;
+      case NavTabAdmin.management:
+        toManagement(context);
+        break;
+      case NavTabAdmin.profil:
+        toProfil(context);
+        break;
+    }
+  }
+}
